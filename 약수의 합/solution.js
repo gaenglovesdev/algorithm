f8007ec8364905311280bc9c504062d09af6850a
gaeng <love@nextm.kr>
2019-05-22 18:20:10 +0900

    약수의합 , 자릿수 더하기, 이상한 문자 만들기

---
+++ 약수의 합/solution.js
@@ -0,0 +1,18 @@
+/**
+ * 자연수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.
+ * 
+ * 제한 사항
+ * - n은 0 이상 3000이하인 자연수입니다.
+ */
+
+function solution(n) {
+    var answer = 0;
+    for(var i=0;i<=n;i++){
+        if(n % i === 0){
+            answer += i;
+        }
+    }
+    return answer;
+}
+
+console.log(solution(5))
